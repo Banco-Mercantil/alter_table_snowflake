5a9201d28f92cbe39e0268a0155e8785b0dc2670
Pamella Fernandes <88452990+pamellamoff@users.noreply.github.com>
2024-05-27 14:35:34 -0300

    Create alter_table.sql

---
+++ alter_table.sql
@@ -0,0 +1,6 @@
+/*Estrutura*/
+  ALTER TABLE table ADD COLUMN new_col VARCHAR DEFAULT 'value';
+
+/*Exemplo*/
+  ALTER TABLE sdx_excelencia_comercial.camp_incentivo_dev.int_metas__individuais ADD COLUMN meta_cartao NUMBER(38,0);
+
